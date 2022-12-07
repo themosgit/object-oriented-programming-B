0d0ddc7712f2ce7d4ae461da689947fa12f517a7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-07 09:16:30 +0200

    Create Method01.java

---
+++ source_code/Method01.java
@@ -0,0 +1,15 @@
+// Method01.java
+//
+
+package aloni.main;
+
+public class Method01 {
+  static void newMethod() {
+      System.out.println("This is my first Java method!");
+}
+  
+   public static void main(String[] args){
+// more instructions
+newMethod();
+}
+} // end of class Main
