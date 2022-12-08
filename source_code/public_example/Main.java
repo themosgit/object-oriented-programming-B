3b1d3cb708fe8001d9304602fb279c3cf1582e99
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-08 21:29:08 +0200

    Create Main.java

---
+++ source_code/public_example/Main.java
@@ -0,0 +1,14 @@
+// Main.java
+// this class belongs to aloni.main package
+
+package aloni.main; // check that the package name is correct
+
+import package02.Second;
+
+
+class Main {
+public static void main(String[] args) {
+    Second Obj = new Second();
+    System.out.println("This is the " + Obj.role + " of " + Obj.team + " speaking!");
+  }
+}
