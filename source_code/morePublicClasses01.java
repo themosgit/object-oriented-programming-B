67b6c284664d366ff1c4f61e0d2e47ac2b4ba142
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-09 00:21:06 +0200

    Create morePublicClasses01.java

---
+++ source_code/morePublicClasses01.java
@@ -0,0 +1,25 @@
+// morePublicClasses01.java
+//
+
+package aloni.main;
+
+// try adding keyword: public
+class Santa {
+   public void say() {
+      System.out.println("Ho ho ho!!!");
+   }
+}
+
+// try adding keyword: public
+class Elf {
+   public void speak() {
+        System.out.println("It's snow time!");
+   }
+}
+
+// try adding keyword: public
+class Main {
+   public static void main(String[] args) {
+       System.out.println("Ho ho ho!");
+   }
+}
