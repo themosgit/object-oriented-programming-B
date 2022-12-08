c445bff5973d793c71bde26047c6084db23cfae4
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-08 21:31:35 +0200

    Create Second.java

---
+++ source_code/public_example/Second.java
@@ -0,0 +1,9 @@
+// Second.java
+// belongs to package: package02
+
+package package02; // check that the package name is correct
+
+public class Second {
+  public String role = "President";
+  public String team = "Edessaikos";
+}
